160bc65e9f29c44fefccc1f094478bd50498244e
MathisMartin31 <mathis.martin31@gmail.com>
2025-11-03 01:08:39 +0100

    Fail miserably at running the animation

--- source/game.c
+++ source/game.c
@@ -205,6 +205,7 @@ static int ante = 0;
 static int money = 0;
 static int score = 0;
 static int temp_score = 0; // This is the score that shows in the same spot as the hand type.
+static bool score_flames_active = false;
 static FIXED lerped_score = 0;
 static FIXED lerped_temp_score = 0;
 
@@ -1072,18 +1073,40 @@ void display_money(int value)
     tte_printf("#{P:%d,%d; cx:0x%X000}$%d", x_offset, MONEY_TEXT_RECT.top, TTE_YELLOW_PB, value);
 }
 
-void display_chips(int value)
+// Show/Hide flaming score effect if we will score
+// more than the required amount or not
+void check_flaming_score()
+{
+    int curr_score = chips * mult;
+    int required_score = blind_get_requirement(current_blind, ante);
+    if (curr_score >= required_score && !score_flames_active)
+    {
+        // start flaming score
+        score_flames_active = true;
+        return;
+    }
+    if (curr_score < required_score && score_flames_active)
+    {
+        // stop flaming score
+        score_flames_active = false;
+    }
+    return;
+}
+
+void display_chips()
 {
     Rect chips_text_rect = CHIPS_TEXT_RECT;
     tte_erase_rect_wrapper(CHIPS_TEXT_RECT);
-    update_text_rect_to_right_align_num(&chips_text_rect, value, OVERFLOW_LEFT);
-    tte_printf("#{P:%d,%d; cx:0x%X000;}%d", chips_text_rect.left, chips_text_rect.top, TTE_WHITE_PB, value);
+    update_text_rect_to_right_align_num(&chips_text_rect, chips, OVERFLOW_LEFT);
+    tte_printf("#{P:%d,%d; cx:0x%X000;}%d", chips_text_rect.left, chips_text_rect.top, TTE_WHITE_PB, chips);
+    check_flaming_score();
 }
 
-void display_mult(int value)
+void display_mult()
 {
     tte_erase_rect_wrapper(MULT_TEXT_RECT);
-    tte_printf("#{P:%d,%d; cx:0x%X000;}%d", MULT_TEXT_RECT.left, MULT_TEXT_RECT.top, TTE_WHITE_PB, value); // Mult
+    tte_printf("#{P:%d,%d; cx:0x%X000;}%d", MULT_TEXT_RECT.left, MULT_TEXT_RECT.top, TTE_WHITE_PB, mult);
+    check_flaming_score();
 }
 
 void display_round(int value)
@@ -1127,8 +1150,8 @@ void set_hand()
     mult = hand.mult;
 
     print_hand_type(hand.display_name);
-    display_chips(chips);
-    display_mult(mult);
+    display_chips();
+    display_mult();
 }
 
 void card_draw()
@@ -1447,8 +1470,8 @@ void game_start()
     display_round(round); // Set the round display
     display_score(score); // Set the score display
 
-    display_chips(chips); // Set the chips display
-    display_mult(mult); // Set the multiplier display
+    display_chips(); // Set the chips display
+    display_mult(); // Set the multiplier display
 
     display_hands(hands); // Hand
     display_discards(discards); // Discard
@@ -1577,8 +1600,8 @@ static void game_playing_process_input_and_state()
 
             chips = 0;
             mult = 0;
-            display_mult(mult);
-            display_chips(chips);
+            display_mult();
+            display_chips();
         }
     }
     else if (play_state == PLAY_ENDED)
@@ -2103,8 +2126,8 @@ static bool check_and_score_joker_for_event(int* iteration_start, Card* played_c
         JokerObject *joker = list_get(jokers, k);
         if (joker_object_score(joker, played_card, joker_event, &chips, &mult, &money, &retrigger))
         {
-            display_chips(chips);
-            display_mult(mult);
+            display_chips();
+            display_mult();
             display_money(money);
 
             return true;
@@ -2215,7 +2238,7 @@ static void played_cards_update_loop(bool* discarded_card, int* played_selection
 
                                 // Relocated card scoring logic here
                                 chips += card_get_value(played[j]->card);
-                                display_chips(chips);
+                                display_chips();
 
                                 // Allow Joker scoring
                                 joker_scored_index = 0;
@@ -2347,6 +2370,42 @@ static void played_cards_update_loop(bool* discarded_card, int* played_selection
     }
 }
 
+static const Rect score_flame_frames[9] = {
+    {27, 20, 29, 20},
+    {27, 21, 29, 21},
+    {27, 22, 29, 22},
+    {27, 23, 29, 23},
+    {27, 24, 29, 24},
+    {27, 25, 29, 25},
+    {27, 26, 29, 26},
+    {27, 27, 29, 27},
+    {27, 28, 29, 28},
+};
+
+static const BG_POINT score_flame_chips = {1, 9};
+
+static void process_flaming_score()
+{
+    static u8 flame_score_frame = 0;
+    
+    // animate flames at 10FPS = change sprites every 6 frames
+    if (score_flames_active)
+    {
+        if (timer % 6 == 0)
+        {
+            // cycling through 8 frames total
+            flame_score_frame = (flame_score_frame + 1) % 8;
+            main_bg_se_copy_rect(score_flame_frames[flame_score_frame + 1], score_flame_chips);
+        }
+    }
+    else
+    {
+        main_bg_se_copy_rect(score_flame_frames[0], score_flame_chips);
+    }
+
+    return;
+}
+
 static void game_playing_ui_text_update()
 {
     static int last_hand_size = 0;
@@ -2368,6 +2427,9 @@ static void game_playing_ui_text_update()
         last_hand_size = hand_get_size();
         last_deck_size = deck_get_size();
     }
+
+    // animate score flames if we ecceed the score requirement
+    process_flaming_score();
 }
 
 static void game_playing_on_update()
@@ -3506,8 +3568,8 @@ static void game_over_on_exit()
 
     display_round(round);
     display_score(score);
-    display_chips(chips);
-    display_mult(mult);
+    display_chips();
+    display_mult();
     display_hands(hands);
     display_discards(discards);
     display_money(money);
