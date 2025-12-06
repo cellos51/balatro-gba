58d9b980d06630f8e6924c46217eb7c70d833ca5
MathisMartin31 <mathis.martin31@gmail.com>
2025-12-06 21:21:04 +0100

    Created sprite bank for all blinds

---
+++ include/blind_gfx.h
@@ -0,0 +1,11 @@
+#ifndef BLIND_GFX_H
+#define BLIND_GFX_H
+
+#include "joker_gfx0.h"
+#include "joker_gfx1.h"
+#include "joker_gfx2.h"
+#include "joker_gfx3.h"
+#include "joker_gfx4.h"
+#include "joker_gfx5.h"
+
+#endif
