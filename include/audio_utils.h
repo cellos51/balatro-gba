f1b411996913fcc80ce275ecc2c9aa66a399b962
MeirGavish <meir.gavish@gmail.com>
2025-12-12 14:22:08 +0200

    Added some more documentation to play_sfx()

--- include/audio_utils.h
+++ include/audio_utils.h
@@ -42,10 +42,12 @@
 
 /**
  * @brief Play a sound effect, wrapper for mmEffectEx()
- *
+ * See https://maxmod.org/ref/functions/mm_sound_effect.html
+ * and https://maxmod.org/ref/functions/mmEffectEx.html
+ * 
  * @param id the sound id to play, from maxmod compiled soundbank.h header
  * @param rate the pitch rate, the default value is @ref MM_BASE_PTCH_RATE
- * @param volume the volume
+ * @param volume the volume ranging from 0 (silent) to 255 (loudest)
  */
 void play_sfx(mm_word id, mm_word rate, mm_byte volume);
 
