1fa83093f97a119369b4dfbfb4aa389d4a797507
MeirGavish <meir.gavish@gmail.com>
2025-12-13 16:10:15 +0200

    Button sound effect (#312)
    * Removed magic number for CARD_FOCUS_SFX_PITCH_OFFSET_RANGE

--- source/sprite.c
+++ source/sprite.c
@@ -273,7 +273,7 @@ void sprite_object_shake(SpriteObject* sprite_object, mm_word sound_id)
     if (sound_id == UNDEFINED)
         return; // If no sound ID is provided, do nothing
 
-    play_sfx(sound_id, MM_BASE_PITCH_RATE);
+    play_sfx(sound_id, MM_BASE_PITCH_RATE, SFX_DEFAULT_VOLUME);
 }
 
 Sprite* sprite_object_get_sprite(SpriteObject* sprite_object)
@@ -291,7 +291,11 @@ void sprite_object_set_focus(SpriteObject* sprite_object, bool focus)
     }
     sprite_object->focused = focus;
 
-    play_sfx(SFX_CARD_FOCUS, MM_BASE_PITCH_RATE + rand() % 512);
+    play_sfx(
+        SFX_CARD_FOCUS,
+        MM_BASE_PITCH_RATE + rand() % CARD_FOCUS_SFX_PITCH_OFFSET_RANGE,
+        SFX_DEFAULT_VOLUME
+    );
     sprite_object->ty = sprite_object->ty + int2fx((focus ? -1 : 1) * SPRITE_FOCUS_RAISE_PX);
 }
 
