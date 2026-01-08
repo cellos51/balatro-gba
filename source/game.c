17e64fc8a0fcdb0774eacc92e7c4642376825b43
STADD <stadd@windowslive.com>
2026-01-08 21:37:22 +0000

    Fix four fingers joker effect stuck after game over

--- source/game.c
+++ source/game.c
@@ -4796,6 +4796,9 @@ static void game_over_on_exit()
         joker_object_destroy(&joker_object);
     }
 
+    //If Four Fingers was owned this run we have to set back the default size
+    straight_and_flush_size = STRAIGHT_AND_FLUSH_SIZE_DEFAULT;
+
     tte_erase_screen();
 
     // For some reason that I haven't figured out yet,
